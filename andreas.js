fc0f178c83862edb8e345d77d609c970db3a47a7
Andreas Eberli <andreas@eberli.me>
2017-04-19 22:21:24 +0200

    Diverse Änderungen

--- andreas.js
+++ andreas.js
@@ -6,7 +6,7 @@
 			addToExportField("SAPLSUU5" + "\t" + "0050	X");
 			addToExportField("\t" + "0000" + "\t" + "\t" + "BDC_CURSOR" + "\t" + "USR02-BNAME");
 			addToExportField("\t" + "0000" + "\t" + "\t" + "BDC_OKCODE" + "\t" + "=PASS");
-			addToExportField("\t" + "0000" + "\t" + "\t" + "USR02-BNAME" + "\t" + "U217646");
+			addToExportField("\t" + "0000" + "\t" + "\t" + "USR02-BNAME" + "\t" + lines[i]);
 			addToExportField("SAPLSUU5" + "\t" + "0400" + "\t" + "X");
 			addToExportField("\t" + "0000" + "\t" + "\t" + "BDC_CURSOR" + "\t" + "G_PASSWORD2");
 			addToExportField("\t" + "0000" + "\t" + "\t" + "BDC_OKCODE" + "\t" + "=TC_RECEIVERS_MARK");
@@ -16,6 +16,9 @@
 			addToExportField("\t" + "0000" + "\t" + "\t" + "G_PASSWORD1" + "\t" + document.getElementById('newPWField').value);
 			addToExportField("\t" + "0000" + "\t" + "\t" + "G_PASSWORD2" + "\t" + document.getElementById('newPWField').value);
 		}
+		document.getElementById('inputTableDiv').style.display = "none"
+		document.getElementById('genButtonDiv').style.display = "none"
+		document.getElementById('outputAreaDiv').style.display = "initial"
 	 }
 	 
 	function addToExportField(newLineText) {
@@ -23,4 +26,16 @@
 		var textValue = field.value;
 		textValue += newLineText + "\n";
 		field.value = textValue;
+	}
+	
+	function inputSanityCheck() {
+		var field1 = document.getElementById('newPWField');
+		var field2 = document.getElementById('userListFiled');
+		var textValue1 = field1.value;
+		var textValue2 = field2.value;
+		
+	if (field1!=null || field1!="",field2!=null || field2!="")
+	  {
+		  document.getElementById('genButtonDiv').style.display = "initial"
+	  }
 	}
