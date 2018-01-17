ebd2abd1d7bd37cda913798abd0d91fa77a23173
root <root@wordpress4.c.cryptic-form-174413.internal>
2018-01-17 14:37:04 +0000

    Anpassungen für AS ABAP 7.5 | Aktuell nicht funktionsfähig

--- andreas.js
+++ andreas.js
@@ -5,18 +5,18 @@
 			//addToExportField("\t" + "0000" + "\t" + "T" + "\t" + "SU01");
 addToExportField("\t" + "0000" + "\t" + "M" + "\t" + "SU01" + "\t" + "U217646" + "\t" + "17012018" + "\t" + "121811");
 addToExportField("\t" + "0000" + "\t" + "T" + "\t" + "SU01" + "\t" + "BS" + "\t" + "AA" + "\t" + "X" + "\t" + "F");
-			addToExportField("SAPLSUU5" + "\t" + "0050	X");
-			addToExportField("\t" + "0000" + "\t" + "\t" + "BDC_CURSOR" + "\t" + "USR02-BNAME");
+			addToExportField("SAPLSUID_MAINTENANCE" + "\t" + "1050	X");
+			addToExportField("\t" + "0000" + "\t" + "\t" + "BDC_CURSOR" + "\t" + "SUID_ST_BNAME-BNAME");
 			addToExportField("\t" + "0000" + "\t" + "\t" + "BDC_OKCODE" + "\t" + "=PASS");
-			addToExportField("\t" + "0000" + "\t" + "\t" + "USR02-BNAME" + "\t" + lines[i]);
-			addToExportField("SAPLSUU5" + "\t" + "0400" + "\t" + "X");
+			addToExportField("\t" + "0000" + "\t" + "\t" + "SUID_ST_BNAME-BNAME" + "\t" + lines[i]);
+			addToExportField("SAPLSUID_MAINTENANCE" + "\t" + "1400" + "\t" + "X");
 			addToExportField("\t" + "0000" + "\t" + "\t" + "BDC_CURSOR" + "\t" + "G_PASSWORD2");
 			addToExportField("\t" + "0000" + "\t" + "\t" + "BDC_OKCODE" + "\t" + "=TC_RECEIVERS_MARK");
-			addToExportField("SAPLSUU5" + "\t" + "0400" + "\t" + "X");
+			addToExportField("SAPLSUU5" + "\t" + "1400" + "\t" + "X");
 			addToExportField("\t" + "0000" + "\t" + "\t" + "BDC_CURSOR" + "\t" + "G_PASSWORD1");
 			addToExportField("\t" + "0000" + "\t" + "\t" + "BDC_OKCODE" + "\t" + "=SAVE");
-			addToExportField("\t" + "0000" + "\t" + "\t" + "G_PASSWORD1" + "\t" + document.getElementById('newPWField').value);
-			addToExportField("\t" + "0000" + "\t" + "\t" + "G_PASSWORD2" + "\t" + document.getElementById('newPWField').value);
+			addToExportField("\t" + "0000" + "\t" + "\t" + "SUID_ST_NODE_PASSWORD_EXT-PASSWORD" + "\t" + document.getElementById('newPWField').value);
+			addToExportField("\t" + "0000" + "\t" + "\t" + "SUID_ST_NODE_PASSWORD_EXT-PASSWORD2" + "\t" + document.getElementById('newPWField').value);
 		}
 		document.getElementById('inputTableDiv').style.display = "none";
 		document.getElementById('genButtonDiv').style.display = "none";
